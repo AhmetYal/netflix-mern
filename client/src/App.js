6541771055bfe557694e2e9d88ee9c6bdc05160a
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-03-15 17:13:39 +0300

    n

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,20 @@ import Login from "./pages/login/Login";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const user = true;
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/watch" element={<Watch />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route exact path="/" element={user ? <Home /> : <Register />} />
+        <Route path="/login" element={!user ? <Login /> : <Home />} />
+        <Route path="/register" element={!user ? <Register /> : <Home />} />
+        {user && (
+          <>
+            <Route path="/movies" element={<Home type="movies" />} />
+            <Route path="/series" element={<Home type="series" />} />
+            <Route path="/watch" element={<Watch />} />
+          </>
+        )}
       </Routes>
     </>
   );
--- client/src/pages/home/Home.js
+++ client/src/pages/home/Home.js
@@ -2,16 +2,45 @@ import Featured from "../../components/featured/Featured";
 import List from "../../components/list/List.jsx";
 import Navbar from "../../components/navbar/Navbar";
 import "./home.scss";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({ type }) => {
+  const [lists, setLists] = useState([]);
+  const [genre, setGenre] = useState(null);
+
+  useEffect(() => {
+    const getRandomLists = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? "?type=" + type : ""}${
+            genre ? "&genre=" + genre : ""
+          }`,
+          {
+            headers: {
+              token:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMzA1MDFjZTUzZGYwZDBhMzI4YmI1YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0NzM0ODY0MCwiZXhwIjoxNjQ3NzgwNjQwfQ.Zmv0UI5eXIL6P64Tx-qkNb-V7xKUODwrIcPIE_7D32w",
+
+              // + JSON.parse(localStorage.getItem("user")).accessToken,
+            },
+          }
+        );
+
+        setLists(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getRandomLists();
+  }, [type, genre]);
+
   return (
     <div className="home">
       <Navbar />
-      <Featured type="movie" />
-      <List />
-      <List />
-      <List />
-      <List />
+      <Featured type={type} setGenre={setGenre} />
+      {lists.map((list, key) => (
+        <List key={key} list={list} />
+      ))}
     </div>
   );
 };
