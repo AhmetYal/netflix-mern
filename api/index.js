f8dab8cc8300f5a5f09edfe6c036bb81968f6004
ahmet yalçınkaya <a.yalcinkaya0@gmail.com>
2022-03-15 12:38:24 +0300

    d23

--- api/index.js
+++ api/index.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
 
 //mongose connection
 dotenv.config({ path: "./config.env" });
 require("./db/conn");
 
 const app = express();
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
 
 app.listen("8800", () => {
   console.log("Backend is running.");
